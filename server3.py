51846136126bf1c24ede9aa9811c95339e9d2c04
root <root@Azariyas.>
2024-08-15 15:01:27 -0400

    user test

--- server3.py
+++ server3.py
@@ -18,6 +18,8 @@
 #  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 #  DEALINGS IN THE SOFTWARE.
 
+
+#user test for push
 import csv
 # from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
 import http.server
